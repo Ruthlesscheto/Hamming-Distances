00cc40b2c72a1f27908535be05172a6ed3f39de9
dimo_ <d.t.dimov@student.utwente.nl>
2025-10-21 16:23:34 +0200

    Changes and added script

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!binbash
+
+echo Results
+python Hamming Distance.py 100 64 10
+printf "\n"
+python Hamming Distance.py 1000 64 10
+printf "\n"
+python Hamming Distance.py 100 89 10
+printf "\n"
+python Hamming Distance.py 1000 89 10
+printf "\n" 
+python Hamming Distance.py 100 256 10
+printf "\n"
+python Hamming Distance.py 1000 256 10
+echo End
+
