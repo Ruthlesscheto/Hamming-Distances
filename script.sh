8f2dd08b3bb9fa09b48254a6786438dd8bfc2886
dimo_ <d.t.dimov@student.utwente.nl>
2025-10-21 18:46:03 +0200

    small change in removing space

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!bin/bash
 
 echo Results
-python Hamming Distance.py 100 64 10
+python HammingDistance.py 100 64 10
 printf "\n"
-python Hamming Distance.py 1000 64 10
+python HammingDistance.py 1000 64 10
 printf "\n"
-python Hamming Distance.py 100 89 10
+python HammingDistance.py 100 89 10
 printf "\n"
-python Hamming Distance.py 1000 89 10
+python HammingDistance.py 1000 89 10
 printf "\n" 
-python Hamming Distance.py 100 256 10
+python HammingDistance.py 100 256 10
 printf "\n"
-python Hamming Distance.py 1000 256 10
+python HammingDistance.py 1000 256 10
 echo End
 
